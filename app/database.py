085d4c6610d488c09d3860d11106cf7b5ad50140
Farid Latrach <farid.latrach@gmail.com>
2026-02-21 20:16:57 -0700

    Normalize UTF-8 formatting for core app files

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-﻿from collections.abc import Generator
+from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
